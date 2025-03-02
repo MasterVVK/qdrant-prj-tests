6bbc718eaf66c30d0aa35662ae1b0daff83bd0d9
MasterVVK <vvk@2140101.ru>
2025-03-02 17:04:05 +0000

    Up

---
+++ RerankingHybridSearch.py
@@ -0,0 +1,5 @@
+from fastembed import TextEmbedding, LateInteractionTextEmbedding, SparseTextEmbedding
+
+dense_embedding_model = TextEmbedding("sentence-transformers/all-MiniLM-L6-v2")
+bm25_embedding_model = SparseTextEmbedding("Qdrant/bm25")
+late_interaction_embedding_model = LateInteractionTextEmbedding("colbert-ir/colbertv2.0")
